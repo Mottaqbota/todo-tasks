e05823fa1f5ab0ef0c52e066a6844114e04b21df
Motta <125099026+Mottaqbota@users.noreply.github.com>
2023-09-22 17:32:57 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,43 +1,84 @@
-// var arrayTasks = [
-//   {
-//     nameTask = "Fazer Café",
-//     isTaksComplete = false
-//   },
-//   {
-//     nameTask = "Arrumar o Quarto",
-//     isTaksComplete = false
-//   }
-// ]
-
-
-const cardTasks = document.getElementById("card-tasks");
+const cardTasks = document.querySelector(".card-tasks");
 const inputTask = document.getElementById("add-input");
+var arrayTasks = [
+    getStorageTasks
+] 
+
+var getStorageTasks = JSON.parse(localStorage.getItem('Tasks'));
+
+// if (localStorage.getItem('Tasks') !== null) {
+//     localStorage.setItem('Tasks', "a")
+// } else {
+//     return;
+// }
+
 
 function addNewTask() {
   if (inputTask.value == "") {
     alert("Insira uma tarefa válida!")
   } else {
     alert("Tarefa Adicionada com sucesso!")
-      cardTasks.innerHTML += `
-        <label class="list-task" for="chk-task">
-          <input type="checkbox" id="chk-task">
-          <p>${inputTask.value}</p>
-          <button onclick="remTask()"><ion-icon name="trash"></ion-icon>REMOVER</button>
-        </label>
+    cardTasks.innerHTML += `
+    <div class="list-task">
+        <input type="checkbox" onclick="completeTask()">
+        <p>${inputTask.value}</p>
+        <button onclick="remTask(taskToRemove)"><ion-icon name="trash"></ion-icon>REMOVER</button>
+    </div>
       `
     }
+    arrayTasks.push(
+        {
+            nameTask: inputTask.value,
+            isTaskComplete: false
+        }
+    ) 
+    
+    var newArrayTasks = JSON.stringify(arrayTasks);
 
+    localStorage.setItem('Tasks', newArrayTasks);
+
+    console.log(JSON.parse(localStorage.getItem('Tasks')));
+}
+
+function completeTask() {
+    if (arrayTasks.isTaskComplete == false) {
+        var resultado = arrayTasks.map(() => ({ isTaskComplete: true }));
+    } else {
+        var resultado = arrayTasks.map(() => ({ isTaskComplete: false }));
+        // arrayTasks.forEach(item => {
+        //     item.isTaskComplete = false;
+        // });
+    }
+      
+    console.log(resultado)
 }
 
 
-const checkbox = document.querySelector(".chk-task");
-const listTask = document.querySelector(".list-task");
 
-function toggleTaskBackground() {
-  if (checkbox.checked) {
-    listTask.classList.add("checked");
-  } else {
-    listTask.classList.remove("checked");
-  }
-}
-checkbox.addEventListener("change", toggleTaskBackground);
+function showTasks() {
+    for(let i = 0; i < arrayTasks.length; i++) {
+        cardTasks.innerHTML += `
+        <div class="list-task">
+            <input type="checkbox">
+            <p>${arrayTasks[i].nameTask}</p>
+            <button onclick="remTask(taskToRemove)"><ion-icon name="trash"></ion-icon>REMOVER</button>
+        </div>
+        `
+    }
+
+}   
+showTasks()
+
+// var getTasks = JSON.parse.localStorage.getItem('Tasks');
+
+// const checkbox = document.querySelector(".chk-task");
+// const listTask = document.querySelector(".list-task");
+
+// function toggleTaskBackground() {
+//   if (checkbox.checked) {
+//     listTask.classList.add("checked");
+//   } else {
+//     listTask.classList.remove("checked");
+//   }
+// }
+// checkbox.addEventListener("change", toggleTaskBackground);
